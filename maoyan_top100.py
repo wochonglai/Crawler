d4a886b87babfe03bb0aa329410f33af0d158746
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-15 14:48:56 +0800

    get_one_page

--- maoyan_top100.py
+++ maoyan_top100.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+import requests
+from requests.exceptions import RequestException
+import re
+import json
+import time
+import random
+from multiprocessing import Pool
+from multiprocessing import Manager
+import functools
+
+
+def get_one_page(url):
+  # 获取一个页面数据
+  headers = {"User-Agent":"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)"
+            }
+  try:
+    response = requests.get(url,headers=headers)
+    if response.status_code == 200:
+      return response.text
+    return None
+  except RequestException:
+    return None
 
