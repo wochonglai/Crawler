564fe8a3430678394e42f951e10ada9a97f43598
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-14 14:54:34 +0800

    Update fiddler_hero.py

--- fiddler_hero.py
+++ fiddler_hero.py
@@ -32,3 +32,30 @@ def hero_imgs_download(url, header):
         if hero_images_path not in os.listdir():
             os.makedirs(hero_images_path)
         urlretrieve(url = hero_photo_url, filename = filename)
+"""
+函数说明:打印所有英雄的名字和ID
+Parameters:
+    url - GET请求地址，通过Fiddler抓包获取
+    header - headers信息
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-08-07
+"""
+def hero_list(url, header):
+	print('*' * 100)
+	print('\t\t\t\t欢迎使用《王者荣耀》出装下助手！')
+	print('*' * 100)
+	req = requests.get(url = url, headers = header).json()
+	flag = 0
+	for each_hero in req['list']:
+		flag += 1
+		print('%s的ID为:%-7s' % (each_hero['name'], each_hero['hero_id']), end = '\t\t')
+		if flag == 3:
+			print('\n', end = '')
+			flag = 0
+            
